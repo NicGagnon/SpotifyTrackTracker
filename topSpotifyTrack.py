8bd20b5fdcf66a5a3309f5fa6e8f201b1ba7f694
Nicolas Gagnon <Nicolas.z.gagnon@gmail.com>
2019-08-13 18:59:50 +0100

    Refactored the code to use the pandas library instead of the other shittier ones

--- topSpotifyTrack.py
+++ topSpotifyTrack.py
@@ -1,72 +1,48 @@
-#Scrape spotify website and log top song per country 
+# Scrape spotify website and log top song per country
 
 from bs4 import BeautifulSoup
 import requests
-import xlsxwriter
-import xlrd
+from openpyxl import load_workbook
+import pandas as pd
 
 from os.path import expanduser
+
 home = expanduser("~")
+path = "{}/Documents/Practice/SpotifyTrackTracker/SpotifyCharts.xlsx".format(home)
 
 URL_front = "https://spotifycharts.com/regional/"
 URL_back = "/daily/latest"
 page = requests.get(URL_front)
-soup = 	BeautifulSoup(page.text, "html.parser")
+soup = BeautifulSoup(page.text, "html.parser")
 
+#Collect initial information
 countries_list = soup.find("div", {"data-type": "country"})
 country_names = [name.text for name in countries_list.select("li")]
 country_abrvs = [abrv.get("data-value") for abrv in countries_list.select("li")]
 date = [name.text for name in soup.find("div", {"data-type": "date"}).select("li")][0].replace('/', '-')
 
+book = load_workbook(path)
+writer = pd.ExcelWriter(path, engine = 'openpyxl')
+writer.book = book
 
-# open the file for reading
-wbRD = xlrd.open_workbook("{}/Documents/Practice/SpotifyTrackTracker/SpotifyCharts.xlsx".format(home))
-sheets = wbRD.sheets()
-
-## Extracting Top Songs from each country
-workbook = xlsxwriter.Workbook("{}/Documents/Practice/SpotifyTrackTracker/SpotifyCharts.xlsx".format(home))
-
-# run through the sheets and store sheets in workbook
-# this still doesn't write to the file yet
-for sheet in sheets: # write data from old file
-    newSheet = workbook.add_worksheet(sheet.name)
-    for row in range(sheet.nrows):
-        for col in range(sheet.ncols):
-            newSheet.write(row, col, sheet.cell(row, col).value)
-
-## Write New Data
-worksheet = workbook.add_worksheet(date)
-bold = workbook.add_format({'bold': True})
-row, column = 0, 0
-worksheet.write(row, column, "Rank\\Country", bold)
-for rank in range(1, 11):
-	row += 1
-	worksheet.write(row, column, rank, bold)
-column += 1
+master_df = pd.DataFrame()
 
 for country in country_abrvs:
-	print(country)
+  print(country)
 
-	#Get URL for each country and collect soup
-	URL = URL_front + country + URL_back
-	country_page = requests.get(URL)
-	country_soup = BeautifulSoup(country_page.text, "html.parser")
+  # Get URL for each country and collect soup
+  URL = URL_front + country + URL_back
+  country_page = requests.get(URL)
+  country_soup = BeautifulSoup(country_page.text, "html.parser")
 
-	#check for invalid countries, and then scrape top ten songs
-	if country_soup.find("div", {"class": "chart-error"}) : continue
-	song_chart = country_soup.find("table", {"class":"chart-table"})
-	songs = [song.text.strip('\n').replace('\n', ' ') for song in song_chart.find_all("td", {"class":"chart-table-track"})][0:10]
-	
-	#write songs to sheet
-	row = 0
-	worksheet.write(row, column, country_names[country_abrvs.index(country)])
-	for song in songs:
-		row += 1
-		worksheet.write(row, column, song)
-	column += 1  
+  # check for invalid countries, and then scrape top ten songs
+  if country_soup.find("div", {"class": "chart-error"}): continue
+  song_chart = country_soup.find("table", {"class": "chart-table"})
+  songs = [song.text.strip('\n').replace('\n', ' ') for song in
+           song_chart.find_all("td", {"class": "chart-table-track"})][0:10]
+  song_df = pd.DataFrame([country_names[country_abrvs.index(country)]] + songs).transpose()
+  master_df = master_df.append(song_df, sort=False)
 
-workbook.close()
-
-
-
-
+master_df.to_excel(writer, date, index=False)
+writer.save()
+writer.close()
